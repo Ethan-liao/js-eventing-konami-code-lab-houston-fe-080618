f8696be2a21d902c67ecb8f759a2b96124fdf846
Ethan-liao <ethan-liao@outlook.com>
2018-09-19 03:44:38 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,42 @@
+// const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+
+// function init() {
+
+
+// }
+
+//up ,up ,down ,down ,left ,right ,left,right,b,a
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+ 
 
-function init() {
-  // Write your JavaScript code inside the init() function
-
+let index = 0;
+ 
+function init(){
+ document.body.addEventListener('keydown', (e) => {
+  const key = parseInt(e.which);
+  console.log('e.key:', e.key)
+  console.log('e.which:', e.which)
+  console.log('e.detail:', e.detail)
+  console.log(index)
+  if (key === code[index]) {
+    index++;
+ console.log(index)
+    if (index === code.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+})
 }
+
+init()
+// document.addEventListener('keydown', onKeyDownHandler(event));
+
+
+// document.addEventListener('keydown', (event) => {
+//   const keyName = event.key;
+//   alert('keydown event\n\n' + 'key: ' + keyName);
+// });
