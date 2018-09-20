031ec58fa7aa7fc4f98bdd5cc60b1dcde74dcb55
Ethan-liao <ethan-liao@outlook.com>
2018-09-20 01:21:55 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,5 @@
 //up ,up ,down ,down ,left ,right ,left,right,b,a
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+// const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 // let userCode = [42]
 
 // function init() {
