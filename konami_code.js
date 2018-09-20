b8260680c526cdfb68012ea661e0474ea06a6a7e
Ethan-liao <ethan-liao@outlook.com>
2018-09-20 01:35:10 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,12 +11,12 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
  
 
-let index = 1;
+// let index = 1;
  
 function init(){
 document.body.addEventListener('keydown', (e) => {
   const key = parseInt(e.which);
-  // let index=0;
+  let index=0;
   console.log('e.which:', e.which)
   
   console.log('index= ', index)
